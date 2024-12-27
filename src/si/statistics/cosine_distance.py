a1a049cbaf66e3c4ee62ffe6f707710e0516fa21
ritanobrega00 <ritanobrega@outlook.pt>
2024-12-27 14:35:13 +0000

    Create cosine_distance.py

---
+++ src/si/statistics/cosine_distance.py
@@ -0,0 +1,32 @@
+import numpy as np
+
+def cosine_distance(x: np.ndarray, y: np.ndarray) -> np.array:
+    """
+    Aim: calculates the cosine distance between a single sample x and multiple samples y
+    Parameters:
+        x: np.ndarray -> single sample
+        y: np.ndarray -> multiple samples
+    Returns:
+        an array containing the distances between X and the various samples in Y.
+    """
+    # compute the dot product between x and each sample in y using np.dot(y, x)
+    dot_product = np.dot(y, x)
+
+    # compute the magnitude (euclidean norm) of x and of each sample in y
+    norm_x = np.linalg.norm(x)
+    norm_y = np.linalg.norm(y, axis=1)
+
+    # Compute cosine similarity and then distance (that is 1 - cosine similarity)
+    cosine_similarity = dot_product / (norm_x * norm_y)
+    cosine_distance = 1 - cosine_similarity
+
+    return cosine_distance
+
+if __name__ == '__main__':
+    x = np.array([1, 2, 3])
+    y = np.array([[1, 2, 3], [4, 5, 6]])
+    our_distance = cosine_distance(x, y)
+    from sklearn.metrics.pairwise import cosine_distances
+    scipy_distance = cosine_distances(x, y)
+    assert np.allclose(our_distance, scipy_distance)
+    print(our_distance, scipy_distance)
