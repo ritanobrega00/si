923bcc921dc1efef2bcfb5ef6a192b86d406f20a
ritanobrega00 <ritanobrega@outlook.pt>
2024-12-27 12:32:43 +0000

    Update f_classification.py

--- src/si/statistics/f_classification.py
+++ src/si/statistics/f_classification.py
@@ -5,13 +5,19 @@ import scipy
 
 
 def f_classification(dataset: Dataset) -> tuple:
-    
-    classes = dataset.get_classes()
+    """
+    Aim: Analysis of Variance of the dataset
+    Input: dataset
+    Output: tuple with F values + tuple with p values
+    """
+    classes = dataset.get_classes() # get the classes of the dataset
 
+    #Group the samples by class in a list
     groups = []
     for class_ in classes:
         mask = dataset.y == class_
         group = dataset.X[ mask , :]
         groups.append(group)
 
+    #Perform the ANOVA
     return scipy.stats.f_oneway(*groups)
