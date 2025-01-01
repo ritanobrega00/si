31460c18fb1c0d4adaabb9fee982b751820eb3a5
ritanobrega00 <ritanobrega@outlook.pt>
2025-01-01 13:30:27 +0000

    Exercise 14

--- src/si/neural_networks/losses.py
+++ src/si/neural_networks/losses.py
@@ -131,3 +131,44 @@ class BinaryCrossEntropy(LossFunction):
         # Avoid division by zero
         p = np.clip(y_pred, 1e-15, 1 - 1e-15)
         return - (y_true / p) + (1 - y_true) / (1 - p)
+
+
+class CategoricalCrossEntropy(LossFunction):
+    """
+    Categorical cross-entropy loss function - applied to multi-class classification problems. 
+    It measures the dissimilarity between predicted class probabilities and true one-hot encoded class labels.
+    """
+
+    def loss(self, y_true: np.ndarray, y_pred: np.ndarray) -> float:
+        """
+        Compute the categorical cross-entropy loss function.
+
+        Parameters:
+        y_true: numpy.ndarray - The true one-hot encoded labels.
+        y_pred: numpy.ndarray - The predicted probabilities for each class.
+
+        Returns: float - The loss value.
+        """
+        # Avoid division by zero
+        p = np.clip(y_pred, 1e-15, 1 - 1e-15)
+        # use of a normalizing factor (1/N, where N is the number of samples) 
+        # often used in loss functions, to ensure that the loss is scaled correctly 
+        # and consistency between different batch sizes 
+        return -np.sum(y_true * np.log(p)) / y_true.shape[0]
+
+    def derivative(self, y_true: np.ndarray, y_pred: np.ndarray) -> np.ndarray:
+        """
+        Compute the derivative of the categorical cross-entropy loss function.
+
+        Parameters:
+        y_true: numpy.ndarray - The true one-hot encoded labels.
+        y_pred: numpy.ndarray - The predicted probabilities for each class.
+
+        Returns: numpy.ndarray - The derivative of the loss function.
+        """
+        # Avoid division by zero
+        p = np.clip(y_pred, 1e-15, 1 - 1e-15)
+        # use of a normalizing factor (1/N, where N is the number of samples)
+        # often used in loss functions, to ensure that the loss is scaled correctly 
+        # and consistency between different batch sizes 
+        return -y_true / p / y_true.shape[0]
