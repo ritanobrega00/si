453e27322a3bfea94db82c199541497d64a0d0e9
ritanobrega00 <ritanobrega@outlook.pt>
2025-01-02 19:52:26 +0000

    testing

--- tests/unit_tests/test_random_forest_classifier.py
+++ tests/unit_tests/test_random_forest_classifier.py
@@ -18,72 +18,73 @@ from si.models.random_forest_classifier import RandomForestClassifier
 class TestRandomForestClassifier(unittest.TestCase):
 
     def setUp(self):
-        self.X = np.array([[1, 2, 3],
-                           [4, 5, 6],
-                           [7, 8, 9],
-                           [10, 11, 12],
-                           [13, 14, 15]])
-        self.y = np.array([0, 1, 0, 1, 0])
-        self.dataset_custom = Dataset(self.X, self.y)
-
         self.csv_iris = os.path.join(DATASETS_PATH, 'iris', 'iris.csv')
         self.dataset_iris = read_csv(filename=self.csv_iris, features=True, label=True)
-        self.csv_cpu = os.path.join(DATASETS_PATH, 'cpu', 'cpu.csv')
-        self.dataset_cpu = read_csv(filename=self.csv_cpu, features=True, label=True)
-        self.csv_bb = os.path.join(DATASETS_PATH, 'breast_bin', 'breast-bin.csv')
-        self.dataset_bb = read_data_file(filename=self.csv_bb, label=True, sep=",")
 
-        self.datasets = [self.dataset_iris, self.dataset_cpu, self.dataset_bb, self.dataset_custom]
-        
-        self.
+        self.iris_train, self.iris_test = train_test_split(self.dataset_iris, test_size=0.2, random_state=42)
+
+        self.random_forest = RandomForestClassifier(n_estimators=5, max_features=2,
+                                                    min_sample_split=2, max_depth=10,
+                                                    mode='gini', seed=42)
 
     def test_fit(self):
-        self.random_forest.fit(self.dataset)
+        fitted = self.random_forest.fit(self.iris_train)
+        self.assertIsInstance(fitted, RandomForestClassifier)
+        self.assertEqual(self.random_forest.n_estimators, 5)
         self.assertEqual(len(self.random_forest.trees), 5)
         for tree, features in self.random_forest.trees:
             self.assertIsInstance(tree, DecisionTreeClassifier)
             self.assertEqual(len(features), 2)
 
     def test_predict(self):
-        self.random_forest.fit(self.dataset)
-        predictions = self.random_forest.predict(self.dataset)
-        self.assertEqual(len(predictions), len(self.dataset.y))
-        self.assertTrue(set(predictions).issubset(set(self.dataset.y)))
-
+        self.random_forest.fit(self.iris_train)
+        predictions = self.random_forest.predict(self.iris_test)
+        self.assertIsInstance(predictions, np.ndarray)
+        self.assertEqual(len(predictions), len(self.iris_test.y))
+                
     def test_score(self):
-        self.random_forest.fit(self.dataset)
-        predictions = self.random_forest.predict(self.dataset)
-        score = self.random_forest.score(self.dataset, predictions)
+        self.random_forest.fit(self.iris_train)
+        score = self.random_forest.score(self.iris_test)
         self.assertGreaterEqual(score, 0)
         self.assertLessEqual(score, 1)
 
-    def test_different_max_features(self):
-        rf = RandomForestClassifier(n_estimators=5, max_features=3, min_sample_split=2, max_depth=3, mode='gini', seed=42)
-        rf.fit(self.dataset)
-        self.assertEqual(len(rf.trees), 5)
-        for tree, features in rf.trees:
-            self.assertEqual(len(features), 3)
+    def test_different_n_estimators(self):
+        for n in [3, 5, 10]:
+            rf = RandomForestClassifier(n_estimators=n, max_features=3, min_sample_split=2,
+                                         max_depth=10, mode='gini', seed=42)
+            rf.fit(self.iris_train)
+            self.assertEqual(len(rf.trees), n)
+            for tree, features in rf.trees:
+                self.assertEqual(len(features), 3)
+                
 
     def test_different_n_estimators(self):
-        rf = RandomForestClassifier(n_estimators=10, max_features=2, min_sample_split=2, max_depth=3, mode='gini', seed=42)
-        rf.fit(self.dataset)
-        self.assertEqual(len(rf.trees), 10)
+        for n_feat in [2, 5, 10]:
+            rf = RandomForestClassifier(n_estimators=8, max_features=n_feat, min_sample_split=2,
+                                         max_depth=10, mode='gini', seed=42)
+            rf.fit(self.iris_train)
+            for tree, features in rf.trees:
+                self.assertEqual(len(rf.trees), 8)
+                self.assertEqual(len(features), n_feat)
 
     def test_different_modes(self):
-        rf_gini = RandomForestClassifier(n_estimators=5, max_features=2, min_sample_split=2, max_depth=3, mode='gini', seed=42)
-        rf_entropy = RandomForestClassifier(n_estimators=5, max_features=2, min_sample_split=2, max_depth=3, mode='entropy', seed=42)
-        rf_gini.fit(self.dataset)
-        rf_entropy.fit(self.dataset)
+        rf_gini = RandomForestClassifier(n_estimators=5, max_features=2, min_sample_split=2,
+                                        max_depth=3, mode='gini', seed=42)
+        rf_entropy = RandomForestClassifier(n_estimators=5, max_features=2, min_sample_split=2,
+                                        max_depth=3, mode='entropy', seed=42)
+        rf_gini.fit(self.iris_train)
+        rf_entropy.fit(self.iris_train)
         self.assertEqual(len(rf_gini.trees), 5)
         self.assertEqual(len(rf_entropy.trees), 5)
 
     def test_random_seed(self):
-        # Test if the seed parameter ensures reproducibility
         rf1 = RandomForestClassifier(n_estimators=3, seed=42)
         rf2 = RandomForestClassifier(n_estimators=3, seed=42)
+        
+        rf1.fit(self.dataset_iris)
+        rf2.fit(self.dataset_iris)
 
-        # Ensure that two models with the same seed produce the same results
-        self.assertEqual(rf1._fit(self.dataset).trees, rf2._fit(self.dataset).trees)
+        self.assertEqual(rf1.trees, rf2.trees)
 
 if __name__ == '__main__':
     unittest.main()
