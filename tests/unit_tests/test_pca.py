8df2532058ec8c1f6ef8ac26062b65f0638054d9
ritanobrega00 <ritanobrega@outlook.pt>
2025-01-02 13:33:09 +0000

    Update test_pca.py

--- tests/unit_tests/test_pca.py
+++ tests/unit_tests/test_pca.py
@@ -9,6 +9,7 @@ from datasets import DATASETS_PATH
 from si.io.csv_file import read_csv
 from si.decomposition.pca import PCA
 from sklearn.decomposition import PCA as PCA_sklearn
+from sklearn.preprocessing import StandardScaler 
 
 class TestPCA(unittest.TestCase):
     def setUp(self):
@@ -20,47 +21,75 @@ class TestPCA(unittest.TestCase):
 
         self.different_n_comp = [2, 3, 4, 5]
     
+    def test_invalid_components(self):
+        for c in [-1, 0]:
+            with self.assertRaises(ValueError):
+                PCA(n_components=c)
+
     def test_pca_fit(self):
         for component in self.different_n_comp:
-                pca = PCA(n_components=component)
-                for dataset in self.datasets:
-                    pca.fit(dataset)
-                    self.assertIsNotNone(pca.mean)
-                    self.assertIsNotNone(pca.components)
-                    self.assertIsNotNone(pca.explained_variance)
-                    self.assertEqual(pca.components.shape[1], component)
-        #Test for n_components = 0
-        pca_0_components = PCA(n_components=0)
-        with self.assertRaises(ValueError):
-            pca_0_components.fit(self.dataset_bb)
-            
-    def test_pca_transform(self):
-        for component in self.different_n_comp:
-                pca = PCA(n_components=component)
-                for dataset in self.datasets:
-                    pca.fit(dataset)
-                    transformed_data = pca.transform(dataset)
-                    self.assertEqual(transformed_data.shape[1], component)
-                    self.assertEqual(transformed_data.shape[0], dataset.shape[0])
-        # Test for a non-fitted PCA
-        pca_non_fit = PCA(n_components=2)
-        with self.assertRaises(AssertionError):
-            pca_non_fit.transform(self.dataset_bb)
-
-            
-        def test_pca_fit_transform(self):
+            pca = PCA(n_components=component)
             for dataset in self.datasets:
-                pca = PCA(n_components=2)
-                pca._fit(dataset)
-                transformed_data = pca._transform(dataset)
-                self.assertEqual(transformed_data.shape[1], 2)
-                self.assertEqual(transformed_data.shape[0], dataset.shape[0])
+                pca.fit(dataset.X)
                 self.assertIsNotNone(pca.mean)
                 self.assertIsNotNone(pca.components)
                 self.assertIsNotNone(pca.explained_variance)
-        
-        def test_normalization:
-            pass
+                self.assertEqual(pca.components.shape[0], component)
+                self.assertEqual(pca.explained_variance.shape[0], component)
+
+    def test_pca_normalization(self):
+        for component in self.different_n_comp:
+            pca = PCA(n_components=component)
+            pca_norm = PCA(n_components=component)
+            for dataset in self.datasets:
+                data_scaled = StandardScaler().fit_transform(dataset.X)
+
+                pca.fit(data_scaled)
+                pca_norm._fit(dataset.X, normalization=True)
+                
+                self.assertIsNotNone(pca_norm.mean)
+                self.assertIsNotNone(pca_norm.components)
+                self.assertIsNotNone(pca_norm.explained_variance)
+                self.assertEqual(pca_norm.components.shape[0], component)
+                self.assertEqual(pca_norm.explained_variance.shape[0], component)
+
+                #Test that the results are the same with normalization and with already normalized data
+                self.assertTrue(np.allclose(pca_norm.explained_variance, pca.explained_variance))
+                self.assertTrue(np.allclose(pca_norm.components, pca.components))           
+            
+    def test_pca_transform(self):
+        for component in self.different_n_comp:
+            pca = PCA(n_components=component)
+            for dataset in self.datasets:
+                pca.fit(dataset.X)
+                transformed_data = pca.transform(dataset.X)
+
+                self.assertEqual(transformed_data.shape, (dataset.X.shape[0], component))
+                
+        # Test for a non-fitted PCA
+        pca_non_fit = PCA(n_components=2)
+        with self.assertRaises(RuntimeError):
+            pca_non_fit.transform(self.dataset_bb.X)
+
+
+    def test_pca_sklearn(self):
+        for component in self.different_n_comp:
+            pca = PCA(n_components=component)
+            pca_sklearn = PCA_sklearn(n_components=component)
+            for dataset in self.datasets:
+                pca.fit(dataset.X)
+                X_transformed = pca_sklearn.fit(dataset.X)
+
+                pca.transform(dataset.X)
+                X_sklearn = pca_sklearn.transform(dataset.X)
+
+                self.assertTrue(np.allclose(pca_sklearn.explained_variance_ratio_, pca.explained_variance))
+                self.assertTrue(np.allclose(pca_sklearn.components_.shape, pca.components.shape))
+
+                self.assertIsInstance(X_transformed, np.ndarray)
+                self.assertEqual(X_transformed.shape, X_sklearn.shape)
+                self.assertTrue(np.allclose(X_transformed, X_sklearn))
+                      
 
 
 if __name__ == '__main__':
