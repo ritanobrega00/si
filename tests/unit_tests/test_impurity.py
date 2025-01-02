c7c21236a5b2a4bd2e468a866a5e21db39528018
ritanobrega00 <ritanobrega@outlook.pt>
2025-01-02 23:11:36 +0000

    Update test_impurity.py

--- tests/unit_tests/test_impurity.py
+++ tests/unit_tests/test_impurity.py
@@ -9,11 +9,13 @@ class TestImpurityFunctions(unittest.TestCase):
         self.dataset_multiclass = np.array([1, 0, 2, 1, 2])  
 
     def test_entropy_impurity(self):
+        self.assertIsInstance(entropy_impurity(self.dataset_pure), float)
         self.assertAlmostEqual(entropy_impurity(self.dataset_pure), 0.0, places=5)
         self.assertAlmostEqual(entropy_impurity(self.dataset_balanced_binary), 1.0, places=5)
         self.assertAlmostEqual(entropy_impurity(self.dataset_multiclass),1.52192809489, places=5)
 
     def test_gini_impurity(self):
+        self.assertIsInstance(gini_impurity(self.dataset_pure), float)
         self.assertAlmostEqual(gini_impurity(self.dataset_pure), 0.0, places=5)
         self.assertAlmostEqual(gini_impurity(self.dataset_balanced_binary), 0.5, places=5)
         self.assertAlmostEqual(gini_impurity(self.dataset_multiclass), 0.6399999999999999, places=5)
